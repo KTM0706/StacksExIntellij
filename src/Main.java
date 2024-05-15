f2829651792290eba08a525315d68c1972173333
Keamogetswe Mafora <keamogetswemafora@gmail.com>
2024-05-15 16:51:01 +0200

    Stack example commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,26 @@
+import java.util.Stack;
+
 public class Main {
+
+    public static void main(String[] args) {
+        String orWord ="Primo";
+        String reversed = ReversedWord (orWord);
+        System.out.println("Original word: "+ orWord);
+        System.out.println("Reversed word; "+ reversed);
+
+    }
+
+    public static String ReversedWord(String word){ //void does not return with out void returns
+        Stack <Character> StackOfChar = new Stack <>();
+        for(int i =0;i<word.length();i++ ){
+            StackOfChar.push(word.charAt(i));
+
+        }
+        String reversed ="";
+        while (!StackOfChar.isEmpty()){
+            reversed +=StackOfChar.pop();
+        }
+        return reversed;
+    }
+
 }
